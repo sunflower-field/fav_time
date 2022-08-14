d4e3d7ff82b2c4d20bfaa4fc7c12422c2f2a332e
sunflower-field <himartmusume@gmail.com>
2022-08-14 15:45:22 +0000

    [Add]投稿、編集、削除、userinfo部分テンプレ

--- app/controllers/publics/post_favtimes_controller.rb
+++ app/controllers/publics/post_favtimes_controller.rb
@@ -2,10 +2,12 @@ class Publics::PostFavtimesController < ApplicationController
   def show
     @post_favtimes = PostFavtime.all
     @post_favtime = PostFavtime.find(params[:id])
+    # @user = @post_favtime.user
   end
 
   def edit
     @post_favtime = PostFavtime.find(params[:id])
+    redirect_to(publics_post_favtimes_path) unless @post_favtime.user == current_user
   end
 
   def update
@@ -21,7 +23,7 @@ class Publics::PostFavtimesController < ApplicationController
   def destroy
    @post_favtime = PostFavtime.find(params[:id])
    @post_favtime.destroy
-   redirect_to publics_post_favtime_path(@post_favtime.id)
+   redirect_to publics_post_favtimes_path(@post_favtime.id)
   end
 
   def new
