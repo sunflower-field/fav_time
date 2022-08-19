c647d66031172fde5e6ad52d97c50a762f4d5edc
sunflower-field <himartmusume@gmail.com>
2022-08-19 15:04:19 +0000

    [Fix]いいね

--- app/controllers/publics/likes_controller.rb
+++ app/controllers/publics/likes_controller.rb
@@ -1,13 +1,13 @@
 class Publics::LikesController < ApplicationController
   def create
-    @post_favtime = PostFavtime.find(params[:post_favtime_id])
+    post_favtime = PostFavtime.find(params[:post_favtime_id])
     like = current_user.likes.new(post_favtime_id: post_favtime.id)
     like.save
     redirect_to publics_post_favtime_path(post_favtime)
   end
 
   def destroy
-    @post_favtime = PostFavtime.find(params[:post_favtime_id])
+    post_favtime = PostFavtime.find(params[:post_favtime_id])
     like = current_user.likes.find_by(post_favtime_id: post_favtime.id)
     like.destroy
     redirect_to publics_post_favtime_path(post_favtime)
