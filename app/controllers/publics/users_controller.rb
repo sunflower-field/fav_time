93ec91150b16be02ba0333a9eb081ce6f52de33e
sunflower-field <himartmusume@gmail.com>
2022-08-27 13:24:40 +0000

    [Fix]マイページpost/indexレイアウト

--- app/controllers/publics/users_controller.rb
+++ app/controllers/publics/users_controller.rb
@@ -2,21 +2,12 @@ class Publics::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @comment = Comment.new
-    # @post_favtimes = PostFavtime.search(params[:keyword])
     @post_favtimes = @user.post_favtimes
-    if @suggest_favtime.nil?
-      @suggest_favtime = nil
-    else
-      @suggest_favtime = PostFavtime.offset( rand(PostFavtime.count) ).first
-    end
+    @suggest_favtime = PostFavtime.offset( rand(PostFavtime.count) ).first
   end
 
-  # def search
-  #   @post_favtimes = PostFavtime.search(params[:keyword])
-  # end
-
   def edit
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -39,7 +30,7 @@ class Publics::UsersController < ApplicationController
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
-  
+
   def unscribe
   end
 
